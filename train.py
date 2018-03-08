7ae69073b6bf4df144b14ba09818c437b0c12b5a
ale3otik <aleksey.zotov@phystech.edu>
2018-03-08 14:25:05 +0300

    really cut twice num of iterations

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         raise FileNotFoundError("word embeddings file was't found")
 
     parser = argparse.ArgumentParser(description='RVAE')
-    parser.add_argument('--num-iterations', type=int, default=120000, metavar='NI',
+    parser.add_argument('--num-iterations', type=int, default=60000, metavar='NI',
                         help='num iterations (default: 60000)')
     parser.add_argument('--batch-size', type=int, default=32, metavar='BS',
                         help='batch size (default: 32)')
